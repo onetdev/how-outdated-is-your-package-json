9b83d4395c85b20d91438017e8a3840f8e97d5e8
Jozsef Koller <orosznyet@gmail.com>
2022-09-02 23:34:33 +0200

    feat(parser): store skipped dep counter

--- src/hooks/usePackageParser.ts
+++ src/hooks/usePackageParser.ts
@@ -9,21 +9,31 @@ type PackageParserProps = {
 
 const usePackageParser = ({ input }: PackageParserProps) => {
   const [dependencies, setDependencies] = useState<DependencyEntry[]>([]);
+  const [skipped, setSkipped] = useState<number>(0);
 
   useEffect(() => {
     try {
       const parsed = JSON.parse(input || "{}");
-      setDependencies([
-        ...sanitizeDependencyArray(parsed.dependencies || {}),
-        ...sanitizeDependencyArray(parsed.devDependencies || {}, true),
-      ]);
+      const depsRaw = parsed.dependencies || {};
+      const devDepsRaw = parsed.devDependencies || {};
+      const deps = sanitizeDependencyArray(depsRaw);
+      const devDeps = sanitizeDependencyArray(devDepsRaw, true);
+      setDependencies([...deps, ...devDeps]);
+      setSkipped(
+        Object.keys({ ...depsRaw, ...devDepsRaw }).length -
+          deps.length -
+          devDeps.length
+      );
     } catch (error) {
+      setDependencies([]);
+      setSkipped(0);
       console.error("Hmm, something is fishy with this input.", error);
     }
   }, [input]);
 
   return {
     dependencies,
+    skipped,
   };
 };
 
