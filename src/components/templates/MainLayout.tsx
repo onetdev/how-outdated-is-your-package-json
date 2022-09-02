da07b7f0245c687e63aa3c53957a672d1ea53c55
Jozsef Koller <orosznyet@gmail.com>
2022-09-02 12:55:36 +0200

    feat(footer): add Riltech logo and link to the footer

--- src/components/templates/MainLayout.tsx
+++ src/components/templates/MainLayout.tsx
@@ -1,32 +1,54 @@
-import { FunctionComponent, ReactNode } from "react";
+import { clsx } from "clsx";
+import Image from "next/image";
+import { ComponentProps, FunctionComponent, PropsWithChildren } from "react";
 
 import styles from "@/styles/Layout.module.css";
 
-type MainLayoutProps = { children?: ReactNode | undefined };
-const MainLayout: FunctionComponent<MainLayoutProps> = ({ children }) => (
-  <div className={styles.container}>
-    <main className={styles.main}>{children}</main>
+type MainLayoutProps = PropsWithChildren<{
+  className?: ComponentProps<"div">["className"];
+}>;
+const MainLayout: FunctionComponent<MainLayoutProps> = ({
+  children,
+  className,
+}) => {
+  const style = clsx([styles.container, className]);
 
-    <footer className={styles.footer}>
-      <a href="https://onet.dev" target="_blank" rel="noopener noreferrer">
-        Jozsef Koller
-      </a>
-      <a
-        href="https://buymeacoffee.com/onetdev"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Sponsor my addiction
-      </a>
-      <a
-        href="https://github.com/orosznyet/how-old-is-my-package"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        GitHub source
-      </a>
-    </footer>
-  </div>
-);
+  const links = [
+    { href: "https://onet.dev", children: "Jozsef Koller" },
+    {
+      href: "https://buymeacoffee.com/onetdev",
+      children: "Sponsor my addiction",
+    },
+    {
+      href: "https://github.com/orosznyet/how-old-is-my-package",
+      children: "GitHub source",
+    },
+    {
+      href: "https://riltech.co",
+      children: (
+        <Image
+          src="/riltech.svg"
+          alt="Riltech"
+          title="Riltech"
+          height="16px"
+          width="16px"
+        />
+      ),
+    },
+  ];
+
+  return (
+    <div className={style}>
+      <main className={styles.main}>{children}</main>
+      <footer className={styles.footer}>
+        {links.map(({ href, children: linkChildren }) => (
+          <a key={href} href={href} target="_blank" rel="noopener noreferrer">
+            {linkChildren}
+          </a>
+        ))}
+      </footer>
+    </div>
+  );
+};
 
 export default MainLayout;
