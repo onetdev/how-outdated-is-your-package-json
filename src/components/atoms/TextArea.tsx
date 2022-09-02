7e5e1921dba5fe5b15ab8b65d1ac10cacc8fe19b
Jozsef Koller <orosznyet@gmail.com>
2022-09-02 23:31:09 +0200

    feat(forms): add textarea component

---
+++ src/components/atoms/TextArea.tsx
@@ -0,0 +1,26 @@
+import { clsx } from "clsx";
+import {
+  ComponentProps,
+  FunctionComponent,
+  PropsWithChildren,
+  PropsWithoutRef,
+  RefObject,
+} from "react";
+
+import styles from "@/components/atoms/TextArea.module.css";
+
+type TextAreaProps = PropsWithChildren<
+  {
+    forwardRef?: RefObject<HTMLTextAreaElement>;
+  } & PropsWithoutRef<ComponentProps<"textarea">>
+>;
+const TextArea: FunctionComponent<TextAreaProps> = ({
+  className = "",
+  forwardRef = null,
+  ...props
+}) => {
+  const classes = clsx(styles.textarea, className);
+  return <textarea ref={forwardRef} className={classes} {...props} />;
+};
+
+export default TextArea;
