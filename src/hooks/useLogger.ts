05b5d9f4678c8f615461b7e157aee6be04234492
Konrad Koller <info@onet.dev>
2024-02-27 15:06:22 +0100

    chore(logger): small formatting change and memo

--- src/hooks/useLogger.ts
+++ src/hooks/useLogger.ts
@@ -1,20 +1,29 @@
+import { useCallback, useMemo } from 'react';
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type LoggerRest = any[];
 export type LoggerOptions = {
   scope?: string;
 };
 const useLogger = ({ scope }: LoggerOptions = {}) => {
-  const formatMessage = (message: string) =>
-    `[${scope ?? 'GENERIC'}] ${message}`;
+  const formatMessage = useCallback(
+    (message: string) => `[${scope ?? 'GENERIC'}] ${message}`,
+    [scope],
+  );
 
-  return {
-    log: (message: string, ...rest: LoggerRest) =>
-      console.log(formatMessage(message), ...rest),
-    error: (message: string, ...rest: LoggerRest) =>
-      console.error(formatMessage(message), ...rest),
-    info: (message: string, ...rest: LoggerRest) =>
-      console.info(formatMessage(message), ...rest),
-  };
+  const logger = useMemo(
+    () => ({
+      log: (message: string, ...rest: LoggerRest) =>
+        console.log(formatMessage(message), ...rest),
+      error: (message: string, ...rest: LoggerRest) =>
+        console.error(formatMessage(message), ...rest),
+      info: (message: string, ...rest: LoggerRest) =>
+        console.info(formatMessage(message), ...rest),
+    }),
+    [formatMessage],
+  );
+
+  return logger;
 };
 
 export default useLogger;
